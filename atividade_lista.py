4ae88d71e49c8afd0d09427d083e2a721ba8a8e9
lind77-ata-ops <lindsaybeltramelo@gmail.com>
2025-05-29 17:08:58 -0300

    Add files via upload

---
+++ atividade_lista.py
@@ -0,0 +1,29 @@
+#  Lista em python
+
+lista  =  [1,2,3,6]
+
+# append(1  dado) extended([varios]) +=(varios) insert(indice,dado) # adicionar
+
+# del lista[indice] remove(valor) pop(indice) pop() deletar o ultimo #deletar
+
+# len() sum() min() max() copy() clear() # verificar
+tamanho = len(lista)
+print(tamanho)
+
+# sum - somar
+lista  =  [1,2,3,6]
+somar = sum(lista)
+print(somar)
+
+menor  =  min(lista)
+print(menor)
+
+maior  =  max(lista)
+print(maior)
+
+copia = lista.copy()
+print(copia)
+
+lista.clear()
+print(lista)
+
---
+++ lista_atividade.py
@@ -0,0 +1,22 @@
+numeros = list(range (2,22,2))
+print(numeros)
+
+numeros = list(range(11))
+print(numeros)
+
+numeros = [1,2,3,4,5,6]
+print(numeros[2])
+
+
+lista = [1,2,3,4,5,6,7]
+lista.append(9)
+print(lista)
+
+lista = [1,2,3,4,5,6,7]
+lista.remove(5)
+print(lista)
+
+lista  =  [1,2,3]
+carros  =  ['lamborghini', 'BMW', 'fusca']
+x =  lista + carros
+print(x)
