61154a4adf5caa106e232143efbb602c65da73ea
lind77-ata-ops <lindsaybeltramelo@gmail.com>
2025-06-04 15:48:58 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,14 @@
+c = 10
+while c > 0:
+	print('teste', c)
+	c = c - 1
+	
+escolha = input('escolha sim ou nao')
+
+while escolha == 'sim':
+	print('ola como vai')
+	escolha = input('escolha sim ou nao ')
+	
+	
+else:
+	print('tchau otario')
