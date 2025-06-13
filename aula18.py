aa51f2d2c21f61d6369fa7358ee5525eafc2b3e4
lind77-ata-ops <lindsaybeltramelo@gmail.com>
2025-06-13 16:00:25 -0300

    Add files via upload

--- aula18.py
+++ aula18.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 
 
+
+
+def display():
+    texto   = input_t.get()
+    mostrar_label.config(text=texto)
+
 janela  =  tk.Tk()
 janela.title('teste')
 
@@ -8,17 +14,21 @@ janela.title('teste')
 janela.geometry('500x500')
 
 
-texto =  tk.Label(janela, text = 'Hello',fg='green', bg='yellow')
+texto =  tk.Label(janela, text = 'Hello',fg='green', bg='yellow', font=('arial', 25))
 texto.pack()
 
 
-input_t =  tk.Entry(janela)
+input_t =  tk.Entry(janela, font=('arial', 20))
 input_t.pack()
 
 
-b_t = tk.Button(janela, text = 'clique aqui')
+b_t = tk.Button(janela, text = 'clique aqui', font=('arial', 20), command=display)
 b_t.pack()
 
+
+
+mostrar_label  = tk.Label(janela, text = 'mostrar',fg='green', bg='yellow', font=('arial', 25))
+mostrar_label.pack()  
 
 
 janela.mainloop()
